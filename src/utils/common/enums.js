2879413b4ce93c8391ac47f8d07ed09ff95be1d6
Sarique <sarique20mh@gmail.com>
2024-12-16 11:16:11 +0530

    Added booking model along with Utils from Flight Service

---
+++ src/utils/common/enums.js
@@ -0,0 +1,18 @@
+const SEAT_TYPE = {
+    ECONOMY: 'economy',
+    PREMIUM_ECONOOMY: 'premium-economy',
+    BUSINESS: 'business',
+    FIRST_CLASS: 'first-class'
+}
+
+const BOOKING_STATUS = {
+    INITIATED: 'initaiated',
+    BOOKED: 'booked',
+    CANCELLED: 'cancelled',
+    PENDING: 'pending'
+}
+
+module.exports = {
+    SEAT_TYPE,
+    BOOKING_STATUS
+}
